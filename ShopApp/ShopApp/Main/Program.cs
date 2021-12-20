7186e9bbb694cadfffea38c8c1d4084d0771ec05
niranda <teslatorgames@gmail.com>
2021-12-20 16:26:09 +0200

    added finished project

---
+++ ShopApp/ShopApp/Main/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShopApp.Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var task = Task.Run(async () => await ParallelHandlerAsync());
+            task.Wait();
+            var result = task.Result;
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> FirstReaderAsync()
+        {
+            var strReader = new StreamReader("hello.txt");
+            return await Task.Run(async () => await strReader.ReadToEndAsync());
+        }
+
+        public static async Task<string> SecondReaderAsync()
+        {
+            var strReader = new StreamReader("world.txt");
+            return await Task.Run(async () => await strReader.ReadToEndAsync());
+        }
+
+        public static async Task<string> ParallelHandlerAsync()
+        {
+            var list = new List<Task<string>>();
+            list.Add(FirstReaderAsync());
+            list.Add(SecondReaderAsync());
+            await Task.WhenAll(list);
+
+            var result = await list[0] + " " + await list[1];
+            return result;
+        }
+    }
+}
